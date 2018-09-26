6f49c1a547444e70e4151ab78eec75104dd1be05
aprilmarini <30900267+aprilmarini@users.noreply.github.com>
2018-09-26 10:11:55 -0400

    Update __init__.py

--- restaurant/check50/__init__.py
+++ restaurant/check50/__init__.py
@@ -16,12 +16,12 @@ class Calculating(Checks):
     @check("compiles")
     def testy(self):
         """All y yields a bill of $21.00"""
-        self.spawn("./restaurant").stdin('y').stdin('y').stdin('y').stdin('y').stdin('y').stdout("The total for your food is $21.00.\n The total with 15% tip is $24.15.\n Thank you for your business!\n", "The total for your food is $21.00.\n The total with 15% tip is $24.15.\n Thank you for your business!\n").exit(0)
+        self.spawn("./restaurant").stdin('y').stdin('y').stdin('y').stdin('y').stdin('y').stdout("\nThe total for your food is $21.00.\n The total with 15% tip is $24.15.\n Thank you for your business!\n", "\nThe total for your food is $21.00.\n The total with 15% tip is $24.15.\n Thank you for your business!\n").exit(0)
         
     @check("compiles")
     def test0(self):
         """0 yields a mean of 0"""
-        self.spawn("./restaurant").stdin("n").stdin("n").stdin("n").stdin("n").stdin("n").stdout("The total for your food is $0.00.\n The total with 15% tip is $0.00.\n Thank you for your business!\n", "The total for your food is $0.00.\n The total with 15% tip is $0.00.\n Thank you for your business!\n").exit(0)
+        self.spawn("./restaurant").stdin("n").stdin("n").stdin("n").stdin("n").stdin("n").stdout("\nThe total for your food is $0.00.\n The total with 15% tip is $0.00.\n Thank you for your business!\n", "\nThe total for your food is $0.00.\n The total with 15% tip is $0.00.\n Thank you for your business!\n").exit(0)
 
 
     @check("compiles")
