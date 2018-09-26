b0dc82dda8b1b63f4cfa034594f49b03679083fb
aprilmarini <30900267+aprilmarini@users.noreply.github.com>
2018-09-26 11:57:27 -0400

    Update __init__.py

--- restaurant/check50/__init__.py
+++ restaurant/check50/__init__.py
@@ -17,6 +17,31 @@ class Calculating(Checks):
     def testy(self):
         """All y yields a bill of $21.00"""
         self.spawn("./restaurant").stdin('y').stdin('y').stdin('y').stdin('y').stdin('y').stdout("Total: \$21.00\n", "Total: \$21.00\n").exit(0)
+    
+    @check("compiles")
+    def testgc(self):
+        """Grilled cheese yields a bill of $5.00"""
+        self.spawn("./restaurant").stdin('y').stdin('n').stdin('n').stdin('n').stdout("Total: \$5.00\n", "Total: \$5.00\n").exit(0)  
+        
+    @check("compiles")
+    def testhd(self):
+        """Hot dog yields a bill of $5.00"""
+        self.spawn("./restaurant").stdin('n').stdin('y').stdin('n').stdin('n').stdout("Total: \$5.00\n", "Total: \$5.00\n").exit(0)
+        
+    @check("compiles")
+    def testnachos(self):
+        """nachos yields a bill of $4.00"""
+        self.spawn("./restaurant").stdin('n').stdin('n').stdin('y').stdin('n').stdout("Total: \$4.00\n", "Total: \$4.00\n").exit(0)
+        
+    @check("compiles")
+    def testhamburger(self):
+        """Hamburger yields a bill of $6.00"""
+        self.spawn("./restaurant").stdin('n').stdin('n').stdin('n').stdin('y').stdin('n').stdout("Total: \$6.00\n", "Total: \$6.00\n").exit(0)
+        
+    @check("compiles")
+    def testcheeseburger(self):
+        """Cheeseburger yields a bill of $7.00"""
+        self.spawn("./restaurant").stdin('n').stdin('n').stdin('n').stdin('y').stdin('y').stdout("Total: \$7.00\n", "Total: \$7.00\n").exit(0)        
         
     @check("compiles")
     def test0(self):
@@ -28,6 +53,3 @@ class Calculating(Checks):
     def test_reject_empty_string(self):
         """rejects a non-numeric input of "" """
         self.spawn("./restaurant").stdin("").reject()
-        
-    def number(num):
-        return "(^|[^\d]){}[^\d]".format(num)
