d13328e1cec347508cfc9059c6db1e68fa80fa94
aprilmarini <30900267+aprilmarini@users.noreply.github.com>
2019-01-18 15:25:33 -0500

    Update __init__.py

--- function/check50/__init__.py
+++ function/check50/__init__.py
@@ -1,1 +1,14 @@
+from check50 import *
 
+
+class Calculating(Checks):
+
+    @check()
+    def exists(self):
+        """function.c exists"""
+        self.require("function.c")
+
+    @check("exists")
+    def compiles(self):
+        """function.c compiles"""
+        self.spawn("clang -std=c11 -o function function.c -lcs50 -lm").exit(0)
