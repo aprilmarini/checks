4ccc0f23abd8c7abdc1c39e36a6c32365812c5f7
aprilmarini <30900267+aprilmarini@users.noreply.github.com>
2019-01-18 15:37:50 -0500

    Update __init__.py

--- function/check50/__init__.py
+++ function/check50/__init__.py
@@ -15,5 +15,5 @@ class Calculating(Checks):
         
     @check("compiles")
     def test1(self):
-        """All y yields a bill of $21.00"""
+        """Test 1"""
         self.spawn("./function").stdout("Square Function!\n").stdin(2).stdout("2 squared is 4\n").stdout("\n").stdout("Minimum Function!\n").stdin(1).stdin(2).stdin(3).stdout("The minimum of the three numbers is: 1\n").stdout("\n").stdout("Quadratic Formula Function!\n").stdin(1).stdin(15).stdin(1).stdout("x = -0.066966, -14.933034\n").exit(0)
